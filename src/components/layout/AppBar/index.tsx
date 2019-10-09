39a477048c579af22848c7cbd701a4fa07f32da5
ramenskiy <ramenskiy@zdorgor.ru>
2019-10-09 16:31:49 +0800

    #192239 review fixes

--- src/components/layout/AppBar/index.tsx
+++ src/components/layout/AppBar/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { AppBar, UserMenu, MenuItemLink, translate } from 'react-admin';
-import { Translate } from 'ra-core';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { TranslationContextProps } from 'ra-core';
 import { QilinLogo } from 'assets/icons';
 
 import styles from './styles';
-
-interface Props {
-  translate: Translate;
-}
 
 interface AppBarProps {
   classes: any;
 }
 
-const MyUserMenu = translate((props: Props) => (
+const MyUserMenu = translate((props: TranslationContextProps) => (
   <UserMenu {...props}>
     <MenuItemLink
       to="/settings"
@@ -50,7 +46,6 @@ const MyAppBar = (props: AppBarProps) => {
         <NotificationsIcon />
       </IconButton>
     </AppBar>
-
   );
 };
 
