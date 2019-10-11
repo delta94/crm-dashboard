f3087cb48d6a15f63ffffebdec0b183bda7cc3e4
ramenskiy <ramenskiy@zdorgor.ru>
2019-10-11 16:24:03 +0800

    add them prop

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Layout } from 'react-admin';
 import AppBar from './AppBar';
 import Menu from './Menu';
+import { lightTheme } from './themes';
 
 const MyLayout = (props: any) => {
+
   return (
     <Layout
       {...props}
       appBar={AppBar}
+      theme={lightTheme}
       menu={Menu}
     />
   );
