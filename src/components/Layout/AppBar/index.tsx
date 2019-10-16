fb635e5e120e270f9a951183938ab6ecdeac32c1
ramenskiy <ramenskiy@zdorgor.ru>
2019-10-16 21:42:18 +0800

    appBar WithStyle props validation & import fixes

--- src/components/Layout/AppBar/index.tsx
+++ src/components/Layout/AppBar/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { AppBar, UserMenu, MenuItemLink, translate } from 'react-admin';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import SettingsIcon from '@material-ui/icons/Settings';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import { Typography, IconButton } from '@material-ui/core';
+import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles';
+import { Settings as SettingsIcon, HelpOutline as HelpOutlineIcon } from '@material-ui/icons';
 import { TranslationContextProps } from 'ra-core';
 import { QilinLogo } from 'assets/icons';
-import { LocaleSwitcher } from 'components';
+import { LocaleSwitcher, Notifications } from 'components';
 
-import styles from './styles';
-
-interface AppBarProps {
-  classes: any;
-}
+const styles = createStyles({
+  spacer: {
+    flex: 1,
+  },
+  switcher: {
+    margin: '0 1em',
+  },
+});
 
 const MyUserMenu = translate((props: TranslationContextProps) => (
   <UserMenu {...props}>
@@ -26,20 +26,18 @@ const MyUserMenu = translate((props: TranslationContextProps) => (
   </UserMenu>
 ));
 
+const MyAppBar = (props: WithStyles<typeof styles>) => {
+  const { classes, ...rest } = props;
 
-const MyAppBar = (props: AppBarProps) => {
-  const { classes } = props;
   return (
-    <AppBar {...props} color="default" userMenu={<MyUserMenu />}>
+    <AppBar {...rest} color="default" userMenu={<MyUserMenu />}>
       <QilinLogo />
       <Typography variant="headline" color="inherit">Qilin CRM</Typography>
       <span className={classes.spacer}></span>
       <IconButton color="inherit" href="https://github.com/qilin/crm-dashboard" target="_blank" rel="noopener">
         <HelpOutlineIcon />
       </IconButton>
-      <IconButton color="inherit">
-        <NotificationsIcon />
-      </IconButton>
+      <Notifications />
       <div className={classes.switcher}>
         <LocaleSwitcher />
       </div>
