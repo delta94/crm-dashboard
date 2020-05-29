503f4d1099a95bb4013137b3605fb972014c13fa
Behuamuh <sven1980@list.ru>
2020-05-29 06:14:24 +0300

    added styles to general

--- src/resources/Games/GameGeneral/components/PlatformTab.tsx
+++ src/resources/Games/GameGeneral/components/PlatformTab.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { SelectInput, TextInput } from 'react-admin';
-import { TranslationContextProps, translate } from 'ra-core';
-import { Typography } from '@material-ui/core';
-import { Grid } from '@material-ui/core/';
+import { translate } from 'ra-core';
+import { Typography, Paper, Grid } from '@material-ui/core';
 import { capitalize } from 'helpers';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import { compose } from 'recompose';
 
-interface Props {
-  type: 'recommended' | 'minimal';
-  platform: string;
-}
+const styles = createStyles({
+  root: {
+    padding: 12,
+  },
+});
 
-const PlatformTab = (props: Props & TranslationContextProps) => {
-  const { type, platform, translate } = props;
+const PlatformTab = (props: any) => {
+  const { type, platform, translate, classes } = props;
   const title = type === 'recommended'
     ? translate('resources.games.fields.supportedPlatforms.recommended')
     : `${capitalize(platform)} ${translate('resources.games.fields.supportedPlatforms.minimal')}`;
 
   return (
-    <>
+    <Paper className={classes.root}>
       <Typography variant="subheading">
         {title}
       </Typography>
@@ -97,8 +99,11 @@ const PlatformTab = (props: Props & TranslationContextProps) => {
         source={`platforms.${platform}.${type}.other`}
         fullWidth
       />
-    </>
+    </Paper>
   );
 };
 
-export default translate<any>(PlatformTab);
+export default compose<any, any>(
+  withStyles(styles),
+  translate,
+)(PlatformTab);
--- src/resources/Games/GameGeneral/index.tsx
+++ src/resources/Games/GameGeneral/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { SimpleForm, TextInput, DateInput, translate } from 'react-admin';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import compose from 'recompose/compose';
 
 import Languages from './components/Languages';
 import Genres from './components/Genres';
@@ -8,37 +11,64 @@ import Features from './components/Features';
 import Controllers from './components/Controllers';
 import Platforms from './components/Platforms';
 
+const styles = createStyles({
+  paper: {
+    padding: 12,
+    width: '100%',
+    marginBottom: 8,
+  },
+});
+
 const GameGeneral = (props: any) => {
-  const { translate } = props;
+  const { translate, classes } = props;
 
   return (
     <SimpleForm redirect="list">
-      <TextInput source="title" label="resources.games.fields.title" fullWidth />
-      <TextInput
-        source="developers"
-        label="resources.games.fields.developers.label"
-        helperText={translate('resources.games.fields.developers.description')}
-        fullWidth
-      />
-      <TextInput
-        source="publishers"
-        label="resources.games.fields.publishers.label"
-        helperText={translate('resources.games.fields.publishers.description')}
-        fullWidth
-      />
-      <Languages />
-      <Genres />
-      <Tags />
-      <DateInput
-        source="releaseDate"
-        label="resources.games.fields.releaseDate.label"
-        helperText={translate('resources.games.fields.releaseDate.description')}
-      />
-      <Features />
-      <Controllers />
-      <Platforms />
+      <Paper className={classes.paper}>
+        <TextInput source="title" label="resources.games.fields.title" fullWidth />
+        <TextInput
+          source="developers"
+          label="resources.games.fields.developers.label"
+          helperText={translate('resources.games.fields.developers.description')}
+          fullWidth
+        />
+        <TextInput
+          source="publishers"
+          label="resources.games.fields.publishers.label"
+          helperText={translate('resources.games.fields.publishers.description')}
+          fullWidth
+        />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Languages />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Genres />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Tags />
+      </Paper>
+      <Paper className={classes.paper}>
+        <DateInput
+          source="releaseDate"
+          label="resources.games.fields.releaseDate.label"
+          helperText={translate('resources.games.fields.releaseDate.description')}
+        />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Features />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Controllers />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Platforms />
+      </Paper>
     </SimpleForm>
   );
 };
 
-export default translate(GameGeneral);
+export default compose(
+  withStyles(styles),
+  translate
+)(GameGeneral);
