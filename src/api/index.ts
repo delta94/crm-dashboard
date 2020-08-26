8ff8467fda687ab55d54dd53d0c24f183e11d667
Behuamuh <sven1980@list.ru>
2020-08-26 15:15:24 +0300

    added settings & store url to game creating

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,6 +7,7 @@ const tagsUrl = `${API_URL}/tags`;
 const genresUrl = `${API_URL}/genres`;
 const featuresUrl = `${API_URL}/features`;
 const languagesUrl = `${API_URL}/localization_languages`;
+const settingsUrl = `${API_URL}/settings`;
 
 export const getDevelopersRequest = createGETRequest(developersUrl);
 export const getPublishersRequest = createGETRequest(publishersUrl);
@@ -14,3 +15,4 @@ export const getTagsRequest = createGETRequest(tagsUrl);
 export const getGenresRequest = createGETRequest(genresUrl);
 export const getFeaturesRequest = createGETRequest(featuresUrl);
 export const getLanguagesRequest = createGETRequest(languagesUrl);
+export const getSettingsRequest = createGETRequest(settingsUrl);
