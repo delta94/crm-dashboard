84134fceeb5d8122544710defd787538817645c3
Behuamuh <sven1980@list.ru>
2020-09-03 04:46:24 +0300

    added tags limit variable

--- src/pages/games/components/Game/components/GameEdit/components/General/components/Tags.tsx
+++ src/pages/games/components/Game/components/GameEdit/components/General/components/Tags.tsx
@@ -7,6 +7,8 @@ import styled from 'styled-components/macro';
 import { Title, Description } from 'pages/games/components/Game/styles';
 import { TagsSelector } from 'admin-library';
 
+const TAGS_LIMIT = 10;
+
 interface Props {
   className?: string;
   value: number[];
@@ -25,12 +27,12 @@ const Tags = (props: Props) => {
   return (
     <Wrapper className={className}>
       <Title>{t('game.fields.tags.label')}</Title>
-      <Description>{t('game.fields.tags.description')}</Description>
+      <Description>{t('game.fields.tags.description', { limit: TAGS_LIMIT })}</Description>
       <TagsSelector
         tags={tags}
         onChange={handleChange}
         selected={value}
-        limit={10}
+        limit={TAGS_LIMIT}
         name="tags"
       />
     </Wrapper>
